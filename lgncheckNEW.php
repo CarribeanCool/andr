fa95a8284dafef80aa8724cff5a843fcf9aaaa96
CarribeanCool <carribean200cool@yahoo.com>
2013-01-30 08:15:01 -0800

    Create lgncheckNEW.php

---
+++ lgncheckNEW.php
@@ -0,0 +1,28 @@
+<?php 
+//connect to the db
+$user = 'trekey_tkeyes';
+$pswd = 'tkeyesdb1';
+$db_database = 'trekey_trekeyes';
+$db_hostname = 'localhost';
+$db_server = mysql_connect($db_hostname, $user, $pswd);
+if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
+mysql_select_db($db_database,$db_server)
+or die("Unable to select database: " . mysql_error());
+$lgnText=$_POST['email'];
+$lgnPsw=$_POST['password'];
+
+$query = sprintf("SELECT * FROM login WHERE email='%s' AND password='%s'", 
+mysql_real_escape_string($lgnText), mysql_real_escape_string($lgnPsw));
+$result = mysql_query($query);
+if(mysql_num_rows($result) > 0)
+{
+echo 1;  // for correct login response
+//$row = mysql_fetch_row($result);
+//session_start();
+//$_SESSION['email'] = $lgnText;
+//$_SESSION['password'] = $lgnPsw;
+//$_SESSION['name'] = $row[1];
+}
+else
+echo 0; // for incorrect login response
+?>
